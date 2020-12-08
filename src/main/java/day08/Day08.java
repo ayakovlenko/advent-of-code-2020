1ff3571867246d009522443fb31b3ffb46edc1f5
tony <ayakovlenko@users.noreply.github.com>
2020-12-08 18:38:44 +0200

    Day 8 part 2 solution

--- src/main/java/day08/Day08.java
+++ src/main/java/day08/Day08.java
@@ -5,24 +5,31 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Day08 {
 
     public static void main(String[] args) throws IOException {
-        var console = Console.load(Path.of("./data/day_08_example.txt"));
+        var console = Console.load(Path.of("./data/day_08.txt"));
 
-        int acc = console.run();
-        System.out.println(acc);
+        System.out.println(console.run());
+        console.reset();
+
+        InstructionMender.findPatch(console.instructions).forEach(console::applyPatch);
+        System.out.println(console.run());
     }
 
     static class Console {
 
-        final ArrayList<Instruction> instructions;
+        private final ArrayList<Instruction> instructions;
 
-        final int[] execCount;
+        private final int[] execCount;
 
-        int acc = 0;
+        private int ip = 0; // instruction pointer
+
+        private int acc = 0;
 
         Console(ArrayList<Instruction> instructions) {
             this.instructions = instructions;
@@ -37,10 +44,9 @@ public class Day08 {
         }
 
         int run() {
-            int ip = 0; // instruction pointer
             while (ip < instructions.size()) {
                 if (++execCount[ip] > 1) {
-                    debug();
+                    // debug();
                     break;
                 }
                 var inst = instructions.get(ip);
@@ -58,19 +64,93 @@ public class Day08 {
             return acc;
         }
 
+        void applyPatch(InstructionPatch patch) {
+            instructions.set(patch.ip, patch.instruction);
+        }
+
+        void reset() {
+            Arrays.fill(this.execCount, 0);
+            this.ip = 0;
+            this.acc = 0;
+        }
+
         void debug() {
             System.out.println(Arrays.toString(execCount));
         }
     }
 
     static record Instruction(Operation op, int arg) {
+
         static Instruction parse(String s) {
             var tokens = s.split(" ");
             return new Instruction(Operation.valueOf(tokens[0].toUpperCase()), Integer.parseInt(tokens[1]));
+        }
+
+        static Instruction nop(int arg) {
+            return new Instruction(Operation.NOP, arg);
+        }
+
+        static Instruction jmp(int arg) {
+            return new Instruction(Operation.JMP, arg);
         }
     }
 
     enum Operation {
         NOP, ACC, JMP
+    }
+
+    static record InstructionPatch(int ip, Instruction instruction) {
+    }
+
+    static class InstructionMender {
+
+        static List<InstructionPatch> findPatch(ArrayList<Instruction> instructions) {
+            var result = new LinkedList<InstructionPatch>();
+            backtrack(instructions, 0, new int[instructions.size()], null, result);
+            return result;
+        }
+
+        private static void backtrack(ArrayList<Instruction> instructions,
+                                      int ip,
+                                      int[] execCount,
+                                      InstructionPatch candidate,
+                                      LinkedList<InstructionPatch> patch) {
+            if (!patch.isEmpty()) {
+                return;
+            }
+
+            if (ip == instructions.size() && candidate != null) {
+                patch.add(candidate);
+                return;
+            }
+
+            if (ip >= instructions.size()) {
+                return;
+            }
+
+            if (++execCount[ip] > 1) {
+                execCount[ip]--;
+                return;
+            }
+
+            var inst = instructions.get(ip);
+            switch (inst.op) {
+                case NOP, ACC -> backtrack(instructions, ip + 1, execCount, candidate, patch);
+                case JMP -> backtrack(instructions, ip + inst.arg, execCount, candidate, patch);
+            }
+            if (candidate == null) {
+                switch (inst.op) {
+                    case NOP -> {
+                        var newCandidate = new InstructionPatch(ip, Instruction.jmp(inst.arg));
+                        backtrack(instructions, ip + inst.arg, execCount, newCandidate, patch);
+                    }
+                    case ACC -> backtrack(instructions, ip + 1, execCount, candidate, patch);
+                    case JMP -> {
+                        var newCandidate = new InstructionPatch(ip, Instruction.nop(inst.arg));
+                        backtrack(instructions, ip + 1, execCount, newCandidate, patch);
+                    }
+                }
+            }
+        }
     }
 }
