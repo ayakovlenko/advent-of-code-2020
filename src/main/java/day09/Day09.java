2a1ed01a73a0bfe43a4d8567a3da23300a434123
tony <ayakovlenko@users.noreply.github.com>
2020-12-09 22:08:12 +0200

    Day 9 part 2 solution

--- src/main/java/day09/Day09.java
+++ src/main/java/day09/Day09.java
@@ -3,38 +3,54 @@ package day09;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalLong;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Day09 {
 
-    static int WINDOW_SIZE = 25;
-
     public static void main(String[] args) throws IOException {
-        var input = parseInputFile(Path.of("./data/day_09.txt"));
+        var p = Path.of("./data/day_09.txt");
+        var input = parseInputFile(p);
+        int windowSize = p.toString().endsWith("_example.txt") ? 5 : 25;
 
-        var window = new Window(input.subList(0, WINDOW_SIZE));
-
-        for (int i = WINDOW_SIZE; i < input.size(); i++) {
+        var window = new Window(input.subList(0, windowSize));
+        var part1 = OptionalLong.empty();
+        for (int i = windowSize; i < input.size(); i++) {
             var value = input.get(i);
             if (!window.check(value)) {
-                System.out.println(value);
+                part1 = OptionalLong.of(value);
                 break;
             }
             window.add(value);
         }
+
+        if (part1.isEmpty()) throw new AssertionError();
+        part1.ifPresent(System.out::println);
+
+        var subarray = subarraySum(input, part1.getAsLong());
+        if (subarray == null) throw new AssertionError();
+        var smallest = subarray.stream().mapToLong(Long::longValue).min();
+        var largest = subarray.stream().mapToLong(Long::longValue).max();
+        var part2 = smallest.stream().flatMap(x -> largest.stream().map(y -> x + y)).findFirst();
+
+        if (part2.isEmpty()) throw new AssertionError();
+        part2.ifPresent(System.out::println);
     }
+
+    // Part 1
 
     static class Window {
 
         private final int size;
 
-        private final LinkedList<Long> values;
+        private final Deque<Long> values;
 
         private final Map<Long, Set<Long>> pairs = new HashMap<>();
 
@@ -92,5 +108,26 @@ public class Day09 {
         try (var lines = Files.lines(p)) {
             return lines.map(Long::valueOf).collect(Collectors.toList());
         }
+    }
+
+    // Part 2
+
+    static Deque<Long> subarraySum(List<Long> xs, long target) {
+        var result = new LinkedList<>(xs.subList(0, 2));
+        var sum = result.stream().mapToLong(Long::longValue).sum();
+        for (int i = 0; i < xs.size(); ) {
+            if (sum == target) {
+                return result;
+            } else if (sum < target) {
+                var x = xs.get(i++);
+                result.addLast(x);
+                sum += x;
+            } else {
+                while (sum > target && xs.size() > 2) {
+                    sum -= result.removeFirst();
+                }
+            }
+        }
+        return null;
     }
 }
